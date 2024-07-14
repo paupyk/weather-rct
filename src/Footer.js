1cfd9b3378a93e115c0d5a41701d4badb9e6801c
Kaylea <kayleapaup@yahoo.com>
2024-07-14 13:48:41 -0500

    Updated links

--- src/Footer.js
+++ src/Footer.js
@@ -4,16 +4,16 @@ export default function Footer() {
   return (
     <div className="Footer">
             This project was coded by{" "}
-      <a href="https://github.com/paupyk" target="_blank">
+      <a href="https://github.com/paupyk" target="_blank" rel="noreferrer">
         Kaylea Paup
       </a>{" "}
       and is
-      <a href="https://github.com/paupyk/weather-rct" target="_blank">
+      <a href="https://github.com/paupyk/weather-rct" target="_blank" rel="noreferrer">
         {" "}
         on GitHub
       </a>{" "}
       and
-      <a href="#" target="_blank">
+      <a href="#" target="_blank" rel="noreferrer">
         hosted on Netlify
       </a>
     </div>
